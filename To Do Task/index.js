a07c8915cd463a0a675df73b010b45cabb3f209f
josephauntonybabu <117811007+josephauntonybabu@users.noreply.github.com>
2023-08-02 16:16:02 +0530

    Add files via upload

---
+++ To Do Task/index.js
@@ -0,0 +1,43 @@
+const box = document.getElementById("box");
+const list = document.getElementById("list");
+
+function addtask(){
+    if(box.value ==='' ){
+        alert("write something");
+    }
+
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = box.value;
+        list.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML ="\u00d7";
+        li.appendChild(span);
+    }
+
+    box.value = "";
+    saveData();
+
+}
+
+list.addEventListener("click", function(e){
+    if(e.target.tagName ==="LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+function saveData(){
+    localStorage.setItem("data",list.innerHTML);
+}
+
+function showTask(){
+    list.innerHTML = localStorage.getItem("data");
+}
+
+showTask();
